51f632facb8a42b10ed591c5720f8f91aef8bd37
xuwusheng <xuwusheng@babytree-inc.com>
2020-07-13 14:07:01 +0800

    feat：add gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -1,4 +1,5 @@
 const loadGruntTasks = require('load-grunt-tasks')
+const bsServer = require('browser-sync').create()
 
 module.exports = grunt => {
   let data = {
@@ -17,6 +18,8 @@ module.exports = grunt => {
     date: new Date()
   }
   grunt.initConfig({
+    // 清除文件
+    clean: ['.temp/**', 'dist/**'],
     // 处理sass
     sass: {
       options: {
@@ -32,6 +35,29 @@ module.exports = grunt => {
             ext: '.css'
           }
         ]
+      }
+    },
+    // 压缩js
+    uglify: {
+      options: {
+        mangle: false
+      },
+      my_target: {
+        files: {
+          '.temp/assets/scripts/vendor.js': ['node_modules/jquery/dist/jquery.js', 'node_modules/popper.js/dist/umd/popper.js', 'node_modules/bootstrap/dist/js/bootstrap.js'],
+        }
+      }
+    },
+    // 压缩css
+    cssmin: {
+      options: {
+        mergeIntoShorthands: false,
+        roundingPrecision: -1
+      },
+      target: {
+        files: {
+          '.temp/assets/styles/vendor.css': ['node_modules/bootstrap/dist/css/bootstrap.css']
+        }
       }
     },
     // 处理js
@@ -74,11 +100,11 @@ module.exports = grunt => {
     // 处理html
     web_swig: {
       options: {
-        swigOptions:{
+        swigOptions: {
           cache: false
         },
-        getData: function(tpl){
-            return data
+        getData: function (tpl) {
+          return data
         }
       },
       your_target: {
@@ -92,15 +118,87 @@ module.exports = grunt => {
         ]
       },
     },
-    // 清除文件
-    clean: ['.temp/**', 'dist/**'],
+    copy: {
+      public: {
+        files: [
+          {
+            expand: true,
+            cwd: 'public',
+            src: ['**'],
+            dest: 'dist'
+          }
+        ]
+      },
+      temp: {
+        files: [
+          {
+            expand: true,
+            cwd: '.temp',
+            src: ['**'],
+            dest: 'dist/'
+          }
+        ]
+      }
+    },
     useref: {
       html: '.temp/*.html',
-      temp: 'dist'
-  }
+      temp: '.temp'
+    },
+    // 服务
+    browserSync: {
+      default_options: {
+        bsFiles: {
+          src: ['.temp/**']
+        },
+        options: {
+          notify: false,
+          port: 3001,
+          server: {
+            baseDir: ['.temp', 'src', 'public'],
+            routes: {
+              '/node_modules': 'node_modules',
+            },
+          },
+        }
+      }
+    },
+    // 监听任务
+    watch: {
+      styles: {
+        files: ['src/assets/styles/**.scss'],
+        tasks: ['sass'],
+        options: {
+          interrupt: true,
+        },
+      },
+      scripts: {
+        files: ['src/assets/scripts/**.js'],
+        tasks: ['babel'],
+        options: {
+          interrupt: true,
+        },
+      },
+      html: {
+        files: ['src/**/*.html'],
+        tasks: ['web_swig'],
+        options: {
+          interrupt: true,
+        },
+      }
+    },
   })
   loadGruntTasks(grunt)
+  // 公共编译任务
+  grunt.registerTask('compile', ['sass', 'babel', 'web_swig'])
 
-  grunt.registerTask('build', ['clean', 'sass', 'babel', 'imagemin', 'web_swig', 'useref'])
+  // 开发任务
+  grunt.registerTask('dev', ['clean', 'compile', 'browserSync'])
+
+  // 上线任务
+  grunt.registerTask('build', ['clean', 'compile', 'imagemin', 'uglify', 'cssmin', 'useref', 'copy'])
+
+  grunt.event.on('watch', function(action, filepath, target) {
+    grunt.log.writeln(target + ': ' + filepath + ' has ' + action)
+  })
 
 }
